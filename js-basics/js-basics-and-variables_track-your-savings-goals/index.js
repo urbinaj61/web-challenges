ac5fb9c652f003b5e14124eb9e97e16df286475d
Joseph <urbinaj61@hotmail.com>
2025-05-28 16:34:41 +0200

    js-basics track-savings-goals complete

--- js-basics/js-basics-and-variables_track-your-savings-goals/index.js
+++ js-basics/js-basics-and-variables_track-your-savings-goals/index.js
@@ -14,3 +14,11 @@ Let's build a small program to track your savings goals!
 
 4. Run your JavaScript program to see how much money you'll have saved after the specified time period.
 */
+
+let startingBalance = 1000;
+let monthlyContribution = 300;
+let months = 12;
+
+let totalSavings = startingBalance + monthlyContribution * months;
+
+console.log(totalSavings);
