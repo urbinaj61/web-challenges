dbc8432cfe3788bc31b1b318d261bbb0cbef5114
Joseph <urbinaj61@hotmail.com>
2025-06-03 16:30:56 +0200

    js-inputs-and-strings change-box-appearance complete

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,18 @@
 console.clear();
+
+const box = document.querySelector("[data-js=box]");
+const colour = document.querySelector("[data-js=input-color]");
+const radius = document.querySelector("[ data-js=input-radius]");
+const rotation = document.querySelector("[data-js=input-rotation]");
+
+colour.addEventListener("click", () => {
+  box.style.backgroundColor = `hsl(${colour.value}, 70%, 60%)`;
+});
+
+radius.addEventListener("click", () => {
+  box.style.borderRadius = `${radius.value}%`;
+});
+
+rotation.addEventListener("click", () => {
+  box.style.transform = `rotate(${rotation.value}deg)`;
+});
