3bcd4d73ed8d0bd4458d90a1b61c590397b01004
Joseph <urbinaj61@hotmail.com>
2025-06-03 15:27:51 +0200

    js-inputs-and-strings intput-to-uppercase complete

--- js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,9 @@
 console.clear();
+
+const input = document.querySelector("[data-js=first-input]");
+const button = document.querySelector("[data-js=button-uppercase]");
+
+button.addEventListener("click", () => {
+  //   input.textContent = input.value.toUppercase();
+  input.value = input.value.toUpperCase();
+});
