365443137eb329bc4196c8cffdc05bbd4189d49c
Joseph <urbinaj61@hotmail.com>
2025-06-04 15:21:16 +0200

    js-create-element toast messages complete

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,12 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const toastMessage = document.createElement("li");
+  toastMessage.textContent = "I am a frog";
+  toastMessage.classList.add("toast-container__message");
+  toastContainer.append(toastMessage);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
