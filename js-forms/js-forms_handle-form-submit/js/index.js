fda90b9363ba6f424ff5ef0c988d181d41c2c844
Joseph <urbinaj61@hotmail.com>
2025-06-04 11:35:24 +0200

    js-forms handle-submit complete

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,15 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const firstName = document.querySelector("#first-name");
+const slider = document.querySelector("#badness");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const userObject = Object.fromEntries(formData);
+  console.log(slider.value + userObject.age);
+  slider.value = console.log(userObject);
+  event.target.reset();
+  firstName.focus();
+});
