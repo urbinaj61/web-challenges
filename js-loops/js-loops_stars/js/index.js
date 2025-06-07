5c0d1cf5964f3d40473a9423de01ccd036a69677
Joseph <joseph.urbina61@hotmail.com>
2025-06-07 09:34:49 +0200

    js-loops stars part three not complete

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,41 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+const renderStars = filledStars => {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
-  // --v-- write or modify code below this line --v--
+  //Part one
+  // for (let i = 1; i <= 5; i++) {
+  //   const star = document.createElement("img");
+  //   star.src = "./assets/star-empty.svg";
+  //   starContainer.append(star);
+  // }
 
-  // --^-- write or modify code above this line --^--
-}
+  //part two
+  // for (let i = 1; i <= 5; i++) {
+  //   const star = document.createElement("img");
 
+  //   i <= filledStars
+  //     ? (star.src = "./assets/star-filled.svg")
+  //     : (star.src = "./assets/star-empty.svg");
+
+  //   starContainer.append(star);
+  // }
+
+  //part three
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement("img");
+    star.src = "./assets/star-empty.svg";
+    star.setAttribute("id", i);
+    starContainer.append(star);
+
+    star.addEventListener("click", () => {
+      const index = Number(star.getAttribute("id"));
+      star.src = "./assets/star-filled.svg";
+
+      //Not finished...........
+    });
+  }
+};
 renderStars();
