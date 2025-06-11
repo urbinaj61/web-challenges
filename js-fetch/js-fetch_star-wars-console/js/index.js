96fe534c4b5762f08e0c01eea0459cb4afa715d5
Joseph <joseph.urbina61@hotmail.com>
2025-06-11 10:47:08 +0200

    js-fetch star-wars-console complete

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,11 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+const fetchData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data.results[7].eye_color);
+};
 
 fetchData();
