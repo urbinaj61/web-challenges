7ca0f89275af34fb57d99d0da6cf1a4e80c254d5
Joseph <joseph.urbina61@hotmail.com>
2025-06-18 16:09:05 +0200

    react state 1 journal-app favourites button complete

--- react-state-1/react-state-1_journal-app-favorite-button/src/components/Entry/index.jsx
+++ react-state-1/react-state-1_journal-app-favorite-button/src/components/Entry/index.jsx
@@ -1,18 +1,18 @@
-import FavoriteButton from "../FavoriteButton";
+import FavouriteButton from "../FavouriteButton";
 import "./Entry.css";
 
 export default function Entry({ motto, notes, date }) {
   return (
-    <article className="entry">
-      <time className="entry__date">{date}</time>
-      <div className="entry__content">
-        <div className="entry__motto-container">
-          <h2 className="entry__motto">
+    <article className='entry'>
+      <time className='entry__date'>{date}</time>
+      <div className='entry__content'>
+        <div className='entry__motto-container'>
+          <h2 className='entry__motto'>
             <q>{motto}</q>
           </h2>
-          <FavoriteButton />
+          <FavouriteButton />
         </div>
-        <p className="entry__notes">{notes}</p>
+        <p className='entry__notes'>{notes}</p>
       </div>
     </article>
   );
