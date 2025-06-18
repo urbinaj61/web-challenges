e12cfdf40568b1b2168f6dce97c53bf1f22d9c5e
Joseph <joseph.urbina61@hotmail.com>
2025-06-18 11:17:45 +0200

    react-nesting fragments complete

---
+++ react-nesting/react-nesting_fragments/src/components/Boxes.jsx
@@ -0,0 +1,13 @@
+import Box from "./Box.jsx";
+
+const Boxes = () => {
+  return (
+    <>
+      <Box color='#007bff' />
+      <Box color='#fc3' />
+      <Box color='#ff3333' />
+    </>
+  );
+};
+
+export default Boxes;
