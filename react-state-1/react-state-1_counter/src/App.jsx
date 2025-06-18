f42b3277489d7ac574aa04c2ad26a717c41b9a96
Joseph <joseph.urbina61@hotmail.com>
2025-06-18 15:44:18 +0200

    reacr state 1 counter complete

--- react-state-1/react-state-1_counter/src/App.jsx
+++ react-state-1/react-state-1_counter/src/App.jsx
@@ -1,24 +1,26 @@
+import { useState } from "react";
+
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="container">
+    <div className='container'>
       <h1>{count}</h1>
-      <div className="button-container">
+      <div className='button-container'>
         <button
-          type="button"
+          type='button'
           onClick={() => {
-            console.log("🤔");
+            setCount(count => count - 1);
           }}
         >
           -
         </button>
         <button
-          type="button"
+          type='button'
           onClick={() => {
-            console.log("🤔");
+            setCount(count => count + 1);
           }}
         >
           +
