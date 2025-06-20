8560234d5f95ed9b2f102033c7d1331909e4a847
Joseph <joseph.urbina61@hotmail.com>
2025-06-20 12:07:23 +0200

    react arrays fruits complete

---
+++ react-arrays/react-with-arrays_fruits/src/data/fruits.js
@@ -0,0 +1,9 @@
+const fruits = [
+  { id: 0, fruitName: "Banana", colour: "card--yellow" },
+  { id: 1, fruitName: "Apple", colour: "card--green" },
+  { id: 2, fruitName: "Mango", colour: "card--red" },
+  { id: 3, fruitName: "Strawberry", colour: "card--red" },
+  { id: 4, fruitName: "Grapes", colour: "card--green" },
+];
+
+export default fruits;
