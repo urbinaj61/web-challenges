b26cf3d68f68f7717b74e085ef18f1187e6047e9
Joseph <joseph.urbina61@hotmail.com>
2025-06-21 09:53:33 +0200

    react-state-2 triplpe-count complete

--- react-state-2/react-state-2_triple-count/src/components/Counter.jsx
+++ react-state-2/react-state-2_triple-count/src/components/Counter.jsx
@@ -4,17 +4,17 @@ export default function Counter() {
   const [count, setCount] = useState(0);
 
   function handleIncrement() {
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
   }
 
   return (
-    <div className="counter">
+    <div className='counter'>
       <h1>Count: {count}</h1>
       <button
-        type="button"
-        className="counter__button"
+        type='button'
+        className='counter__button'
         onClick={handleIncrement}
       >
         Increment count by 3
