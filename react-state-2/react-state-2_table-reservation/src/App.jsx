667556942bc2036629a5395a09b623c9ed1ccb74
Joseph <joseph.urbina61@hotmail.com>
2025-06-21 08:07:10 +0200

    react-state-2 table-reservation complete

--- react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  const handleAddPeople = () => {
+    setPeople(people + 1);
+  };
+
+  const handleRemovePeople = () => {
+    setPeople(people - 1);
+  };
+
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        onHandleAddPeople={handleAddPeople}
+        onHandleRemovePeople={handleRemovePeople}
+      />
+      <p>
+        You are going to reserve a table for {people}{" "}
+        {people > 1 ? "people" : "person"}
+      </p>
     </div>
   );
 }
--- react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
+++ react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
@@ -1,25 +1,21 @@
-import { useState } from "react";
-
-export default function Counter() {
-  const [people, setPeople] = useState(0);
-
+export default function Counter({ onHandleAddPeople, onHandleRemovePeople }) {
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
-      <div className="counter__buttons-container">
+      <div className='counter__buttons-container'>
         <button
-          type="button"
-          className="counter__button"
-          aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
+          type='button'
+          className='counter__button'
+          aria-label='increment people count'
+          onClick={onHandleAddPeople}
         >
           +
         </button>
         <button
-          type="button"
-          className="counter__button"
-          aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
+          type='button'
+          className='counter__button'
+          aria-label='decrement people count'
+          onClick={onHandleRemovePeople}
         >
           -
         </button>
