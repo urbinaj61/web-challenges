a058e112eadfac0965cdee82bf6f8900b8d56365
Joseph <joseph.urbina61@hotmail.com>
2025-06-21 09:48:13 +0200

    react-state-2 ersonal-details complete

--- react-state-2/react-state-2_personal-details/src/App.jsx
+++ react-state-2/react-state-2_personal-details/src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleInputs = (receivedName, receivedEmail) => {
+    setName(receivedName);
+    setEmail(receivedEmail);
+  };
+
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Personal Details Form</h1>
-      <Form />
+      <Form onHandleInputs={handleInputs} />
       <h2>Your submitted details:</h2>
       <p>
-        Name: <span className="output">John Doe</span>
+        Name: <span className='output'>{name}</span>
       </p>
       <p>
-        Email: <span className="output">John@Doe.com</span>
+        Email: <span className='output'>{email}</span>
       </p>
     </div>
   );
