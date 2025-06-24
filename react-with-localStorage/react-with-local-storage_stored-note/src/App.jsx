122ba00c2534d912b5c538416194463adcf5ea8e
Joseph <joseph.urbina61@hotmail.com>
2025-06-24 11:07:19 +0200

    react-with-localStorage stored-note complete

--- react-with-localStorage/react-with-local-storage_stored-note/src/App.jsx
+++ react-with-localStorage/react-with-local-storage_stored-note/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import FontSelector from "./components/FontSelector";
 import Note from "./components/Note";
 import "./styles.css";
 
 export default function App() {
-  const [note, setNote] = useState("");
-  const [font, setFont] = useState("system-ui");
+  const [note, setNote] = useLocalStorageState("note", { defaultValue: "" });
+  const [font, setFont] = useLocalStorageState("font", { defaultValue: "" });
+
+  // setFont(localStorage["font"] && localStorage.getItem(JSON.parse("font")));
 
   function handleNoteChange(newNote) {
     setNote(newNote);
@@ -16,7 +18,7 @@ export default function App() {
   }
 
   return (
-    <div className="app" style={{ "--font": font }}>
+    <div className='app' style={{ "--font": font }}>
       <FontSelector font={font} onFontChange={handleFontChange} />
       <Note note={note} onNoteChange={handleNoteChange} />
     </div>
