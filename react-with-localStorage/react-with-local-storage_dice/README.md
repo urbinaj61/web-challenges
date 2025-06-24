f7140377d26c8e3cc48a448172fede9c585940aa
Joseph <joseph.urbina61@hotmail.com>
2025-06-24 11:38:06 +0200

    react-with-localStorage dice complete

--- react-with-localStorage/react-with-local-storage_dice/README.md
+++ react-with-localStorage/react-with-local-storage_dice/README.md
@@ -15,7 +15,7 @@ Lift up the `rolls` state into the `App` component.
 The `D6Button` component should have the following props:
 
 - `value` which represents the current roll value (because the button only needs to know the most recent value)
-- `onRoll` which is a function that get called on roll (when the button is clicked)
+- `onRoll` which is a function that gets called on roll (when the button is clicked)
 
 > 💡 When designing the interface of your component (which props it has), think about what the component needs to know to function properly. In this case the component only needs to know the current value it should show, and a function that gets called when the button is clicked. Hence you should not pass the whole `rolls` array to the component.
 
--- react-with-localStorage/react-with-local-storage_dice/src/App.jsx
+++ react-with-localStorage/react-with-local-storage_dice/src/App.jsx
@@ -1,15 +1,27 @@
+import useLocalStorageState from "use-local-storage-state";
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
+import { getD6Roll } from "./utils";
 import "./styles.css";
 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", {
+    defaultValue: [],
+  });
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+
+  const currentRollValue = rolls[0]?.value;
+
   return (
-    <div className="app">
-      <main className="app__main">
-        <D6Button />
+    <div className='app'>
+      <main className='app__main'>
+        <D6Button onRoll={handleRoll} value={currentRollValue} />
       </main>
-      <aside className="app__aside">
-        <History rolls={[]} />
+      <aside className='app__aside'>
+        <History rolls={rolls} />
       </aside>
     </div>
   );
