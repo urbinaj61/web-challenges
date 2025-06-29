4b5482075f51e68d6c135c3e9c21f51c4d17350d
Joseph <joseph.urbina61@hotmail.com>
2025-06-29 05:56:13 +0200

    prettier not working. Fixed

--- nextjs-basics/lotr-app/pages/the-return-of-the-king.jsx
+++ nextjs-basics/lotr-app/pages/the-return-of-the-king.jsx
@@ -1,34 +1,38 @@
-
-import  { useRouter }  from "next/router"
-import   Image  from "next/image"
-import { volumes } from "../f25e013e-5e94-48da-9c90-a0e315ad4db1/lib/data.js"
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { volumes } from "../f25e013e-5e94-48da-9c90-a0e315ad4db1/lib/data.js";
 
 const TheReturnOfTheKing = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-    const data = volumes?.find(({slug}) => slug === "the-return-of-the-king")
+  const data = volumes?.find(({ slug }) => slug === "the-return-of-the-king");
 
-    return (
-        <>
-        <h1>{data.title}</h1>
-        <p>{data.description}</p>
-        <ul>
-            {data.books.map(book => {
-                return <li key={book.ordinal}>
-                {book.title}, {book.ordinal}
-              </li>
-            })}
-        </ul>
+  return (
+    <>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <ul>
+        {data.books.map((book) => {
+          return (
+            <li key={book.ordinal}>
+              {book.title}, {book.ordinal}
+            </li>
+          );
+        })}
+      </ul>
 
-        <Image  
-                src={data.cover}
-                width={140}
-                height={230}
-                alt="The return of the King"/>
-       
-        <button type="button" onClick={() => router.back()}>Previous Page</button>
-        </>
-        )
-}
+      <Image
+        src={data.cover}
+        width={140}
+        height={230}
+        alt="The return of the King"
+      />
 
-export default TheReturnOfTheKing
+      <button type="button" onClick={() => router.back()}>
+        Previous Page
+      </button>
+    </>
+  );
+};
+
+export default TheReturnOfTheKing;
