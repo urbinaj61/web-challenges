e4584ba73e1ac7756f1f9cecb84629348dc94017
Joseph <joseph.urbina61@hotmail.com>
2025-07-01 10:37:27 +0200

    JS unit testing get animal complete

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  } else if (animal === "dogs") {
+    return `I like ${animal}!`;
+  } else if (animal === undefined) {
+    return "I do not like animals at all!";
+  }
+}
