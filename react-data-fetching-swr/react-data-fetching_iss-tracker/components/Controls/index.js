b711321bbf70b6854f7a28fa7526d11ef6d29174
Joseph <joseph.urbina61@hotmail.com>
2025-07-03 15:54:39 +0200

    tried to implement mutate. Failed

--- react-data-fetching-swr/react-data-fetching_iss-tracker/components/Controls/index.js
+++ react-data-fetching-swr/react-data-fetching_iss-tracker/components/Controls/index.js
@@ -4,13 +4,13 @@ import {
   ControlsDisplay,
 } from "./Controls.styled";
 
-export default function Controls({ longitude, latitude, onRefresh, mutate }) {
+export default function Controls({ longitude, latitude }) {
   return (
     <ControlsContainer>
       <ControlsDisplay>Lat: {longitude.toFixed(5)}</ControlsDisplay>
       <ControlsDisplay>Long: {latitude.toFixed(5)}</ControlsDisplay>
-      {/* <ControlsButton type='button' onClick={() => onRefresh()}>
-        Refresh
+      {/* <ControlsButton type='button' onClick={onFresh}>
+        refresh
       </ControlsButton> */}
     </ControlsContainer>
   );
