0d8f2b7c1b3cbe84edab06fc95bed1f7aee2737d
Joseph <joseph.urbina61@hotmail.com>
2025-07-09 16:41:28 +0200

    backend-api-routes random character complete

--- backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,21 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then(response => response.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error, isLoading } = useSWR(`/api/random-character/`, fetcher);
+
+  if (error) console.log("There is an error");
+  if (isLoading) return <h1>Loading.....</h1>;
+
+  const { firstName, lastName, twitter, geohash } = data;
+
+  return (
+    <>
+      <p>{firstName}</p>
+      <p>{lastName}</p>
+      <p>{twitter}</p>
+      <p>{geohash}</p>
+    </>
+  );
 }
